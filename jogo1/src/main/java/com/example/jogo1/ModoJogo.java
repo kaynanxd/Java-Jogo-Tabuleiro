13ca2be09d56cabc358eb96a98b81bbc28384a6b
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-06-15 22:44:54 -0300

    Delete jogo1/src/main/java/com/example directory

--- jogo1/src/main/java/com/example/jogo1/ModoJogo.java
+++
@@ -1,8 +0,0 @@
-package com.example.jogo1;
-
-
-public abstract class ModoJogo {
-	public abstract Dados.ModoJogo getModoJogo();
-
-	public abstract void realizarJogada(Jogador jogadorAtual, JogoTabuleiro jogoTabuleiro);
-}
--- jogo1/src/main/java/com/example/jogo1/Teste.java
+++
@@ -1,7 +0,0 @@
-package com.example.jogo1;
-public class Teste {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-    }
-}
-
