77a205e29201ff191cc692efe4ad10c5c0ee683d
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-06-29 20:47:05 -0300

    Update Debug.java

--- jogo1/src/main/java/com/example/jogo1/Debug.java
+++ jogo1/src/main/java/com/example/jogo1/Debug.java
@@ -2,12 +2,9 @@
 package com.example.jogo1;
 
 public class Debug extends ModoJogo {
-    @Override
     public Dados.ModoJogo getModoJogo() {
         return Dados.ModoJogo.DEBUG; 
     }
-
-    @Override
     public void realizarJogada(Jogador jogadorAtual, JogoTabuleiro jogoTabuleiro) {
         jogoTabuleiro.obterValoresDadosDebug(jogadorAtual);
     }
