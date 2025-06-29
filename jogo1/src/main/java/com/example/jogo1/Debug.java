1abc15b98be073c51c20b663651a4520d4f46f0d
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-06-29 20:42:52 -0300

    Update Debug.java

--- jogo1/src/main/java/com/example/jogo1/Debug.java
+++ jogo1/src/main/java/com/example/jogo1/Debug.java
@@ -4,12 +4,11 @@ package com.example.jogo1;
 public class Debug extends ModoJogo {
     @Override
     public Dados.ModoJogo getModoJogo() {
-        return Dados.ModoJogo.DEBUG; // Correto: Referencia o enum de Dados
+        return Dados.ModoJogo.DEBUG; 
     }
 
     @Override
     public void realizarJogada(Jogador jogadorAtual, JogoTabuleiro jogoTabuleiro) {
-        // No modo DEBUG, o JogoTabuleiro é responsável por obter os valores dos dados do usuário.
         jogoTabuleiro.obterValoresDadosDebug(jogadorAtual);
     }
 }
